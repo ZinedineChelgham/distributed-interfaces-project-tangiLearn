9e6c17e4ed618a9ce0db6158832812b41a05761d
Weel06 <60885609+weelBenAissa@users.noreply.github.com>
2023-12-01 21:13:29 +0100

    + et - dans les cases

--- monitoring/src/components/gameboard.js
+++ monitoring/src/components/gameboard.js
@@ -1,19 +1,33 @@
 // GameBoard.js
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react';
 import './gameboard.css';
 
 const GameBoard = () => {
-    const renderCells = () => {
-        const cells = [];
-        const totalCells = 9;
+    const [cellValues, setCellValues] = useState(Array(9).fill(0));
 
-        for (let i = 1; i <= totalCells; i++) {
-            cells.push(<div key={i} className="grid-item"></div>);
-        }
+    const handleIncrement = (index) => {
+        const newCellValues = [...cellValues];
+        newCellValues[index] = Math.min(newCellValues[index] + 1, 4);
+        setCellValues(newCellValues);
+    };
 
-        return cells;
-    }
+    const handleDecrement = (index) => {
+        const newCellValues = [...cellValues];
+        newCellValues[index] = Math.max(newCellValues[index] - 1, 0);
+        setCellValues(newCellValues);
+    };
+
+        const renderCells = () => {
+            return cellValues.map((value, index) => (
+                <div key={index} className="grid-item">
+                    <button onClick={() => handleDecrement(index)}>-</button>
+                    <span>{value}</span>
+                    <button onClick={() => handleIncrement(index)}>+</button>
+                </div>
+            ));
+        };
+
+
 
     return (
         <div className="grid-container">
@@ -21,9 +35,5 @@ const GameBoard = () => {
         </div>
     );
 }
-
-GameBoard.propTypes = {
-    gridSize: PropTypes.number.isRequired,
-};
 
 export default GameBoard;
