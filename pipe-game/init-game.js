9e38ddf20af829e47088edb2af81e3a3dcb86626
zinedine <zinedine.chelgham@etu.unice.fr>
2023-12-07 18:15:58 +0100

    image added in the center of the inner circles.

--- pipe-game/init-game.js
+++ pipe-game/init-game.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const innerCircle = document.querySelector('.inner-circle');
-    const outerCircle = document.querySelector('.outer-circle');
+document.addEventListener("DOMContentLoaded", () => {
+  const innerCircle = document.querySelector(".container");
+  const outerCircle = document.querySelector(".outer-circle");
 
-    let isGreen = false;
+  let isGreen = false;
 
-    innerCircle.addEventListener('click', function() {
-        if (isGreen) {
-            outerCircle.style.backgroundColor = 'grey';
-            isGreen = false;
-        } else {
-            outerCircle.style.backgroundColor = 'green';
-            isGreen = true;
-        }
-    });
+  innerCircle.addEventListener("click", () => {
+    if (isGreen) {
+      outerCircle.style.backgroundColor = "grey";
+      isGreen = false;
+    } else {
+      outerCircle.style.backgroundColor = "green";
+      isGreen = true;
+    }
+  });
 });
