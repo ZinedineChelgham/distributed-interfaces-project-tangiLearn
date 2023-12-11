003c93b26bfb363f6fdca1372a6e374f3756770a
Joel <joel@dibasso.fr>
2023-12-11 15:49:41 +0100

    Rotation sticky test

--- pipe-game/index.js
+++ pipe-game/index.js
@@ -7,9 +7,6 @@ const BOARD_HEIGHT = 800;
 const IMAGES_WIDTH = 100;
 const IMAGES_HEIGHT = 100;
 
-const placeableImgsInRow = BOARD_WIDTH / IMAGES_WIDTH;
-const placeableImgsInCol = BOARD_HEIGHT / IMAGES_HEIGHT;
-
 const inventoryWidth = (1920 - BOARD_WIDTH) / 2;
 
 let gameContainer = document.getElementById("game-container");
@@ -17,7 +14,6 @@ const gameContainerBoundingRect = gameContainer.getBoundingClientRect();
 new TUIOManager(gameContainer).start();
 
 
-let draggedItem = null;
 let pipeCounts = {
   1: 5,
   2: 5,
@@ -31,62 +27,6 @@ let cells = [];
 
 initInventory();
 
-// initBoard();
-
-function initBoard() {
-  for (let i = 0; i < placeableImgsInRow; i++) {
-    for (let j = 0; j < placeableImgsInCol; j++) {
-      let cell = document.createElement("div");
-      cell.id = i + "-" + j;
-      cell.classList.add("cell");
-      cell.addEventListener("dragover", function(event) {
-        event.preventDefault();
-      });
-      cell.addEventListener("dragenter", function(event) {
-        event.preventDefault();
-      });
-      cell.addEventListener("drop", function(event) {
-        if (!cell.children.length && draggedItem) {
-          let clone = draggedItem.cloneNode(true);
-          clone.classList.add("rotation-0");
-          clone.style.transition = "transform 0.4s ease"; // Adding transition to the transform property
-          clone.addEventListener("dragstart", function() {
-            draggedItem = clone;
-            setTimeout(() => {
-              //draggedItem.style.display = "flex";
-            }, 0);
-          });
-
-          clone.addEventListener("dragend", function(e) {
-            console.log(e);
-            setTimeout(() => {
-              //remove from the dom
-              draggedItem.remove();
-              draggedItem = null;
-            }, 0);
-          });
-
-          clone.addEventListener("click", function(e) {
-            let curAngle = 0;
-            clone.classList.forEach((className) => {
-              if (className.startsWith("rotation-")) {
-                clone.classList.remove(className);
-                curAngle = parseInt(className.split("-")[1]);
-              }
-            });
-            curAngle += 90 % 360;
-            clone.style.transform = "rotate(" + curAngle + "deg)";
-            clone.classList.add("rotation-" + curAngle);
-          });
-
-          cell.appendChild(clone);
-        }
-      });
-      document.getElementById("board").appendChild(cell);
-    }
-  }
-}
-
 for (let i = 0; i < BOARD_WIDTH / IMAGES_WIDTH; i++) {
   for (let j = 0; j < BOARD_HEIGHT / IMAGES_HEIGHT; j++) {
     const cell = document.createElement("div");
@@ -98,60 +38,35 @@ for (let i = 0; i < BOARD_WIDTH / IMAGES_WIDTH; i++) {
   }
 }
 
-function getCategory(pipe) {
-  let pipeCat;
-  pipe.domElem.get(0).classList.forEach((className) => {
-    if (className.startsWith("pipe-cat-")) {
-      pipeCat = className.split("-")[2];
-    }
-  });
-  return pipeCat;
-}
-
 function getNewCurvedPipe() {
   const pipe = new PipeElementWidget(
-    inventoryWidth/2 + gameContainerBoundingRect.left - IMAGES_WIDTH/2,
+    inventoryWidth / 2 + gameContainerBoundingRect.left - IMAGES_WIDTH / 2,
     300,
-    IMAGES_WIDTH,
-    IMAGES_HEIGHT,
-    0,
-    1,
-    "./assets/images/pipe_curved.svg"
+    "curved"
   );
   pipe.addTo(board);
-  pipe.domElem.get(0).classList.add("pipe-cat-1");
   addPipeListeners(pipe);
   return pipe;
 }
 
 function getNewStraightPipe() {
   const pipe = new PipeElementWidget(
-    inventoryWidth/2 + gameContainerBoundingRect.left - IMAGES_WIDTH/2,
+    inventoryWidth / 2 + gameContainerBoundingRect.left - IMAGES_WIDTH / 2,
     500,
-    IMAGES_WIDTH,
-    IMAGES_HEIGHT,
-    0,
-    1,
-    "./assets/images/pipe_straight.svg"
+    "straight"
   );
   pipe.addTo(board);
-  pipe.domElem.get(0).classList.add("pipe-cat-2");
   addPipeListeners(pipe);
   return pipe;
 }
 
 function getNewTShapePipe() {
   const pipe = new PipeElementWidget(
-    inventoryWidth/2 + gameContainerBoundingRect.left - IMAGES_WIDTH/2,
-    750,
-    IMAGES_WIDTH,
-    IMAGES_HEIGHT,
-    0,
-    1,
-    "./assets/images/pipe_t_shape.svg"
+    inventoryWidth / 2 + gameContainerBoundingRect.left - IMAGES_WIDTH / 2,
+    700,
+    "t-shape"
   );
   pipe.addTo(board);
-  pipe.domElem.get(0).classList.add("pipe-cat-3");
   addPipeListeners(pipe);
   return pipe;
 }
@@ -167,11 +82,10 @@ function addPipeListeners(pipe) {
     pipe.domElem.get(0).style.left = `${tuioTag.x}px`;
     pipe.domElem.get(0).style.top = `${tuioTag.y}px`;
     if (tuioTag.angle !== pipe.tagCurrentAngle) {
-      pipe.domElem.get(0).style.transform = `rotate(${tuioTag.angle*180/Math.PI}deg)`;
+      pipe.domElem.get(0).style.transform = `rotate(${tuioTag.angle * 180 / Math.PI}deg)`;
       pipe.angle = tuioTag.angle;
       pipe.tagCurrentAngle = tuioTag.angle;
     }
-    console.log(tuioTag);
   });
 
   pipe.onTagDeletion((tuioTagId) => {
@@ -189,21 +103,21 @@ function addPipeListeners(pipe) {
     const cellY = closestCell.coords.y;
     pipe.domElem.get(0).style.left = cellX + "px";
     pipe.domElem.get(0).style.top = cellY + "px";
-
     pipe.domElem.get(0).classList.remove(`drag-${tuioTagId}`);
-    let pipeCat = getCategory(pipe);
-    pipeCounts[pipeCat] = pipeCounts[pipeCat] - 1;
+
+    const closestAngle = Math.round(pipe.angle * 180 / Math.PI / 90) * 90;
+    pipe.domElem.get(0).style.transform = `rotate(${closestAngle}deg)`;
     // countSpan.textContent = `*${pipeCounts[pipeCat] || 0}`;
     console.log(pipeCounts);
-    switch (pipeCat) {
-      case "1":
-        if (pipeCounts[pipeCat] > 0)
+    switch (pipe.category) {
+      case "curved":
+        if (pipeCounts[pipe.category] > 0)
           getNewCurvedPipe();
         break;
-      case "2":
+      case "straight":
         getNewStraightPipe();
         break;
-      case "3":
+      case "t-shape":
         getNewTShapePipe();
         break;
     }
@@ -211,26 +125,7 @@ function addPipeListeners(pipe) {
 }
 
 function initInventory() {
-  getNewCurvedPipe(),
-    getNewStraightPipe(),
-    getNewTShapePipe();
-
-  // for (let i = 0; i < pipes.length; i++) {
-  //   const pipe = pipes[i];
-  //   const pipeCat = pipe.getAttribute("data-pipeCat");
-  //   const pipeCount = pipeCounts[pipeCat];
-  //   const countSpan = document.createElement("span");
-  //   countSpan.classList.add("pipeCount");
-  //   countSpan.textContent = `*${pipeCount || 0}`;
-  //   inventoryItems[i].appendChild(countSpan);
-  //
-
-  //
-  //   pipe.addEventListener("dragend", function() {
-  //     setTimeout(() => {
-  //       //draggedItem.style.display = "block";
-  //       draggedItem = null;
-  //     }, 0);
-  //   });
-  // }
+  getNewCurvedPipe();
+  getNewStraightPipe();
+  getNewTShapePipe();
 }
--- pipe-game/src/pipeElementWidget.js
+++ pipe-game/src/pipeElementWidget.js
@@ -1,8 +1,27 @@
 import { ImageElementWidget } from "@dj256/tuiomanager/widgets";
 
 export class PipeElementWidget extends ImageElementWidget {
-  constructor(x, y, width, height, initialRotation, initialScale, src) {
-    super(x, y, width, height, initialRotation, initialScale, src);
+  constructor(x, y, type) {
+    let src = ""
+    let category = ""
+    switch (type) {
+      case "straight":
+        src = "./assets/images/pipe_straight.svg"
+        category = "straight"
+        break;
+      case "curved":
+        src = "./assets/images/pipe_curved.svg"
+        category = "curved"
+        break;
+      case "t-shape":
+        src = "./assets/images/pipe_t_shape.svg"
+        category = "t-shape"
+        break;
+      default:
+        break;
+    }
+    super(x, y, 100, 100, 0, 1, src);
+    this.category = category
     this.angle = 0
     this.tagCurrentAngle = 0
   }
