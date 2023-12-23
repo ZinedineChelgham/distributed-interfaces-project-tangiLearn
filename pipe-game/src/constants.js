553068cde2b800503ec28ab50e623e40b04fa235
Joël DIBASSO <51450344+dj-256@users.noreply.github.com>
2023-12-23 18:30:46 +0100

    Disable touch events for pipe widgets (#7)
    * Clean code a little
    * Extract all constants to a dedicated file
    * Override touch callbacks to disable the `ImageWidget` default touch event handling

---
+++ pipe-game/src/constants.js
@@ -0,0 +1,12 @@
+export const GAME_WIDTH = 1920;
+export const GAME_HEIGHT = 1080;
+export const IMAGE_SIZE = 100;
+export const BOARD_WIDTH = 14 * IMAGE_SIZE;
+export const BOARD_HEIGHT = 8 * IMAGE_SIZE;
+
+export const PIPE_TYPES = {
+  CURVED: "curved",
+  STRAIGHT: "straight",
+  T_SHAPE: "t-shape",
+  LONG: "long",
+};
