00d6c0e27ec0b9459f9f3e4c6c15d2c0d117b909
ZC <zinedine.chelgham@etu.univ-cotedazur.fr>
2023-12-25 17:50:27 +0100

    Game intro working

--- pipe-game/init-game.js
+++ pipe-game/init-game.js
@@ -1,16 +1,88 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const innerCircle = document.querySelector(".container");
-  const outerCircle = document.querySelector(".outer-circle");
+import { TUIOManager } from "@dj256/tuiomanager";
+import { ImageElementWidget } from "@dj256/tuiomanager/widgets";
 
-  let isGreen = false;
+const managerAnchor = document.querySelector("body");
+new TUIOManager(managerAnchor).start();
 
-  innerCircle.addEventListener("click", () => {
-    if (isGreen) {
-      outerCircle.style.backgroundColor = "grey";
-      isGreen = false;
-    } else {
+const outerCircles = document.querySelectorAll(".outer-circle");
+const containers = document.querySelectorAll(".container");
+
+function createMagnetImage(
+  x,
+  y,
+  width,
+  height,
+  angle,
+  zIndex,
+  imagePath,
+  container,
+  classToAdd,
+) {
+  const img = new ImageElementWidget(
+    x,
+    y,
+    width,
+    height,
+    angle,
+    zIndex,
+    imagePath,
+  );
+  img.canMove(false, false);
+  img.domElem.get(0).classList.add(classToAdd);
+  img.addTo(container);
+  return img;
+}
+
+const magnetTopImg = createMagnetImage(
+  60,
+  60,
+  80,
+  80,
+  -43,
+  1,
+  "./assets/images/magnet.png",
+  containers[0],
+  "magnet-top",
+);
+const magnetBopImg = createMagnetImage(
+  60,
+  60,
+  80,
+  80,
+  137,
+  1,
+  "./assets/images/magnet.png",
+  containers[1],
+  "magnet-bot",
+);
+
+const containersState = { top: false, bot: false };
+
+function setupMagnetEvents(magnetImg, outerCircle, stateKey) {
+  magnetImg.onTagCreation(() => {
+    if (!containersState[stateKey]) {
       outerCircle.style.backgroundColor = "green";
-      isGreen = true;
+      containersState[stateKey] = true;
+      checkAndNavigate();
     }
   });
-});
+
+  magnetImg.onTagDeletion(() => {
+    if (containersState[stateKey]) {
+      outerCircle.style.backgroundColor = "grey";
+      containersState[stateKey] = false;
+    }
+  });
+}
+
+function checkAndNavigate() {
+  if (containersState.top && containersState.bot) {
+    //wait 500ms before navigating to index.js
+    setTimeout(() => {
+      window.location.href = "index.html"; // Navigate to index.js
+    }, 500);
+  }
+}
+
+setupMagnetEvents(magnetTopImg, outerCircles[0], "top");
+setupMagnetEvents(magnetBopImg, outerCircles[1], "bot");
