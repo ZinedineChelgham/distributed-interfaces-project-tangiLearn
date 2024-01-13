09e5ed6c9aa4f2dc416c0d0bfb10b5a4ddac51c8
Joël DIBASSO <51450344+dj-256@users.noreply.github.com>
2024-01-13 20:14:56 +0100

    Restyle entry menu (#23)
    * Restyle the entry menu
    * Make entry-menu a vite project
    * Install sass
    * Change style according to the Figma designs
    * Add logo to the menu

---
+++ entry-menu/main.js
@@ -0,0 +1,38 @@
+import "./style.scss";
+
+const BACKEND_URL = "http://localhost:3000";
+
+// Get the IP address and port separately
+const ipAddress = BACKEND_URL.substring(0, BACKEND_URL.lastIndexOf(":"));
+
+const GAME_URL_MAPPER = {
+  pipe: `${ipAddress}:5174/`,
+  tower: `${ipAddress}:5173/`,
+};
+
+const API = `${BACKEND_URL}/api`;
+
+function checkGameStatus() {
+  fetch(`${API}/monitoring/current-game`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const gameName = data;
+      if (!gameName) return;
+
+      if (gameName === "tower") {
+        fetch(`${API}/tower-game/get-id`)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            if (data.gameId) {
+              window.location.href =
+                GAME_URL_MAPPER[gameName] + "gamepage?id=" + data.gameId;
+            }
+          });
+      } else window.location.href = GAME_URL_MAPPER[gameName];
+    })
+    .catch((error) => console.error("Error checking game status:", error));
+}
+
+setInterval(checkGameStatus, 1000);
