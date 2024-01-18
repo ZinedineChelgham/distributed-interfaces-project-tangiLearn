40f0d078f4a5ebd3609d8cbce0e4728782319f2b
Joel <joel@dibasso.fr>
2024-01-18 19:54:04 +0100

    Add .env files to tower game

--- jeu-des-tours/src/util.js
+++ jeu-des-tours/src/util.js
@@ -1,3 +1,2 @@
 export const BACKEND_URL =
-    // eslint-disable-next-line no-undef
-    import.meta.env.VITE_APP_BACKEND_URL ?? "http://192.168.1.14:3000";
+  import.meta.env.VITE_BACKEND_URL ?? "http://192.168.1.14:3000";
