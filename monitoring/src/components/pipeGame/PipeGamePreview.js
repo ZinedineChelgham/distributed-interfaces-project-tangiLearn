afbdd9be4e24dea78282d7db2bc2c5386ed07e6f
ZC <zinedine.chelgham@etu.univ-cotedazur.fr>
2024-01-19 19:27:58 +0100

    monitoring: Seeing who's playing live

--- monitoring/src/components/pipeGame/PipeGamePreview.js
+++ monitoring/src/components/pipeGame/PipeGamePreview.js
@@ -8,46 +8,95 @@
  * @typedef PipeGameState {{type: PipeType, rotation: 0 | 90 | 180 | 270}[][]}
  */
 import PipeGameBoard from "./PipeGameBoard";
-import { useEffect, useState } from "react";
-import { BACKEND_URL } from "../../util";
+import react, {useEffect, useState} from "react";
+import {BACKEND_URL} from "../../util";
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Tooltip from '@mui/material/Tooltip';
+import TowerGameBoard from "../towerGame/TowerGameBoard";
 
-function PipeGamePreview({ gameId }) {
-  const [state, setState] = useState(undefined);
+function PipeGamePreview({gameId}) {
+    const [state, setState] = useState(undefined);
+    const [players, setPlayers] = react.useState([{
+        name: 'John',
+        surname: 'Doe',
+        avatar: 'https://i.pravatar.cc/150?img=3'
+    }]);
 
-  useEffect(() => {
-    const id = setInterval(
-      () =>
-        fetch(`${BACKEND_URL}/api/pipe-game/${gameId}`)
-          .then((response) => response.json())
-          .then((game) => setState(game.state))
-          .catch((error) =>
-            console.error("Error checking game status:", error),
-          ),
-      1000,
+    const test = [{
+        name: 'John',
+        surname: 'Doe',
+        avatar: 'https://i.pravatar.cc/150?img=3'
+    }]
+
+    useEffect(() => {
+        const id = setInterval(
+            () =>
+                fetch(`${BACKEND_URL}/api/pipe-game/${gameId}`)
+                    .then((response) => response.json())
+                    .then((game) => setState(game.state))
+                    .catch((error) =>
+                        console.error("Error checking game status:", error),
+                    ),
+            1000,
+        );
+
+        return () => clearInterval(id);
+    }, []);
+
+    useEffect(() => {
+        // Function to fetch current players
+        const fetchPlayers = () => {
+            fetch(`${BACKEND_URL}/api/monitoring/current-players`)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("Network response was not ok");
+                    }
+                    return response.json(); // Assuming the response is JSON
+                })
+                .then((data) => {
+                    // Update state with the retrieved current players
+                    setPlayers(data);
+                })
+                .catch((error) => {
+                    console.error("Error fetching current players:", error);
+                });
+        };
+
+        // Call the function when the component mounts
+        fetchPlayers();
+
+        // Set an interval to call the function every 5 seconds
+        const interval = setInterval(fetchPlayers, 5000);
+
+        // Clear the interval when the component unmounts
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <>
+            {state && state.board && state.board[0] && (
+                <Box display={'flex'} alignItems={'center'} justifyContent={'center'} flexDirection={'column'}>
+                    <Box width={'100%'}>
+                        <PipeGameBoard
+                            pipeGameState={state}
+                            dimensions={{rows: 8, columns: 14}}
+                            cellSize={50}
+                        />
+                    </Box>
+
+                    <Box display={'flex'} alignItems={'center'} justifyContent={'center'} direction={'row'}>
+                        {players.map((player) => (
+                            <Tooltip key={player.name} title={`${player.name} ${player.surname}`} placement="top" arrow>
+                                <Avatar alt={`${player.name} ${player.surname}`} src={player.avatar} sx={{m: 1}}/>
+                            </Tooltip>
+                        ))}
+                    </Box>
+                </Box>
+            )}
+
+        </>
     );
-
-    return () => clearInterval(id);
-  }, []);
-  return (
-    <>
-      {state && state.board && state.board[0] && (
-        <PipeGameBoard
-          pipeGameState={state}
-          dimensions={{ rows: 8, columns: 14 }}
-          cellSize={50}
-        />
-      )}
-      <div className="players">
-        {state &&
-          state.players &&
-          state.players.map((player) => (
-            <div key={player.id} className="player">
-              <div className="name">{player.name}</div>
-            </div>
-          ))}
-      </div>
-    </>
-  );
 }
 
 export default PipeGamePreview;
--- monitoring/src/components/towerGame/TowerGamePreview.js
+++ monitoring/src/components/towerGame/TowerGamePreview.js
@@ -1,12 +1,20 @@
 import react, {useEffect} from "react";
 import TowerGameBoard from "./TowerGameBoard";
 import {BACKEND_URL} from "../../util";
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Tooltip from '@mui/material/Tooltip';
 
 function TowerGamePreview() {
 
     const [gameState, setGameState] = react.useState([]);
     const [selectedValues, setSelectedValues] = react.useState([]);
     const [gameId, setGameId] = react.useState('');
+    const [players, setPlayers] = react.useState([{
+        name: 'John',
+        surname: 'Doe',
+        avatar: 'https://i.pravatar.cc/150?img=3'
+    }]);
 
     useEffect(() => {
         fetch(`${BACKEND_URL}/api/tower-game/get-id`)
@@ -32,6 +40,35 @@ function TowerGamePreview() {
         return () => clearInterval(intervalId);
     }, [gameId]);
 
+    useEffect(() => {
+        // Function to fetch current players
+        const fetchPlayers = () => {
+            fetch(`${BACKEND_URL}/api/monitoring/current-players`)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("Network response was not ok");
+                    }
+                    return response.json(); // Assuming the response is JSON
+                })
+                .then((data) => {
+                    // Update state with the retrieved current players
+                    setPlayers(data);
+                })
+                .catch((error) => {
+                    console.error("Error fetching current players:", error);
+                });
+        };
+
+        // Call the function when the component mounts
+        fetchPlayers();
+
+        // Set an interval to call the function every 5 seconds
+        const interval = setInterval(fetchPlayers, 5000);
+
+        // Clear the interval when the component unmounts
+        return () => clearInterval(interval);
+    }, []);
+
     const firstEntryValues = selectedValues || [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     const paramToCaseMapping = {
         "01": firstEntryValues[0],
@@ -51,12 +88,24 @@ function TowerGamePreview() {
     return (
         <>
             {gameState.length > 0 && selectedValues.length > 0 && (
-                <TowerGameBoard
-                    gameState={gameState}
-                    dimensions={{rows: 5, columns: 5}}
-                    cellSize={80}
-                    paramToCaseMapping={paramToCaseMapping}
-                />
+                <Box display={'flex'} alignItems={'center'} justifyContent={'center'} flexDirection={'column'}>
+                    <Box width={'100%'}>
+                        <TowerGameBoard
+                            gameState={gameState}
+                            dimensions={{rows: 5, columns: 5}}
+                            cellSize={70}
+                            paramToCaseMapping={paramToCaseMapping}
+                        />
+                    </Box>
+
+                    <Box display={'flex'} alignItems={'center'} justifyContent={'center'} direction={'row'}>
+                        {players.map((player) => (
+                            <Tooltip key={player.name} title={`${player.name} ${player.surname}`} placement="top" arrow>
+                                <Avatar alt={`${player.name} ${player.surname}`} src={player.avatar} sx={{m: 1}}/>
+                            </Tooltip>
+                        ))}
+                    </Box>
+                </Box>
             )}
         </>
     );
